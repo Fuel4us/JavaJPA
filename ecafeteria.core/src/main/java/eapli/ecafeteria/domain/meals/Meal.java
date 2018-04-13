073fed596509dd6871b0c9d04f3d23f5c2dbed42
Berccar <Berccar@LAPTOP-FM6PBNT3>
2018-04-13 01:00:45 +0100

    Criação classe MealType na ecafeteria.core Desenvolvimento classe Meal

--- ecafeteria.core/src/main/java/eapli/ecafeteria/domain/meals/Meal.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/domain/meals/Meal.java
@@ -5,10 +5,60 @@
  */
 package eapli.ecafeteria.domain.meals;
 
+import eapli.framework.domain.Designation;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Version;
+
 /**
  *
- * @author Berccar
+ * @author Bernardo Carreira
  */
+@Entity
 public class Meal {
-    
+    private static final long serialVersionUID = 1L;
+
+    @Version
+    private Long version;
+
+    @EmbeddedId
+    private Designation name;
+    /**
+     * cascade = CascadeType.NONE as the dishType is part of another aggregate
+     */
+    @ManyToOne()
+    private MealType mealType;
+    private NutricionalInfo nutricionalInfo;
+    private Money price;
+    private boolean active;
+
+    public Dish(final DishType dishType, final Designation name,
+            final NutricionalInfo nutricionalInfo, Money price) {
+        if (dishType == null || name == null || nutricionalInfo == null) {
+            throw new IllegalArgumentException();
+        }
+
+        this.dishType = dishType;
+        this.name = name;
+        this.nutricionalInfo = nutricionalInfo;
+        this.setPrice(price);
+        this.active = true;
+    }
+
+    public Dish(final DishType dishType, final Designation name, Money price) {
+        if (dishType == null || name == null || price == null) {
+            throw new IllegalArgumentException();
+        }
+
+        this.dishType = dishType;
+        this.name = name;
+        this.nutricionalInfo = null;
+        this.price = price;
+        this.active = true;
+    }
+
+    protected Dish() {
+        // for ORM only
+    }
 }
---
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/domain/meals/MealType.java
@@ -0,0 +1,176 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eapli.ecafeteria.domain.meals;
+
+import eapli.framework.domain.ddd.AggregateRoot;
+import eapli.framework.util.Strings;
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+/**
+ *
+ * @author Bernardo Carreira
+ */
+@Entity
+public class MealType implements AggregateRoot<String>, Serializable { //implementa AggregateRoot?
+    
+    private static final long serialVersionUID = 1L;
+
+    // ORM primary key
+    @Id
+    @GeneratedValue
+    private Long pk;
+    @Version
+    private Long version;
+
+    // business ID
+    @Column(unique = true)
+    private String acronym;
+    private String description;
+    private boolean active;
+
+    protected MealType() {
+        // for ORM
+    }
+
+    /**
+     * DishType constructor.
+     *
+     * @param name
+     *            Mandatory
+     * @param description
+     *            Mandatory
+     */
+    public MealType(String name, String description) {
+        setName(name);
+        setDescription(description);
+        this.active = true;
+    }
+
+    /**
+     * Sets and validates newDescription.
+     *
+     * @param newDescription
+     */
+    private void setDescription(String newDescription) {
+        if (descriptionMeetsMinimumRequirements(newDescription)) {
+            this.description = newDescription;
+        } else {
+            throw new IllegalArgumentException("Invalid Description");
+        }
+    }
+
+    /**
+     * Sets and validates newName.
+     *
+     * @param newName
+     */
+    private void setName(String newName) {
+        if (nameMeetsMinimumRequirements(newName)) {
+            this.acronym = newName;
+        } else {
+            throw new IllegalArgumentException("Invalid Name");
+        }
+    }
+
+    /**
+     * Ensure name is not null or empty.
+     *
+     * @param name
+     * @return True if name meets minimum requirements. False if name does not
+     *         meet minimum requirements.
+     */
+    private boolean nameMeetsMinimumRequirements(String name) {
+        return !Strings.isNullOrEmpty(name);
+    }
+
+    /**
+     * Ensure description is not null or empty.
+     *
+     * @param description
+     * @return True if description meets minimum requirements. False if
+     *         description does not meet minimum requirements.
+     */
+    private boolean descriptionMeetsMinimumRequirements(String description) {
+        return !Strings.isNullOrEmpty(description);
+    }
+
+    public String description() {
+        return this.description;
+    }
+
+    public boolean isActive() {
+        return this.active;
+    }
+
+    /**
+     * Toggles the state of the mealType, activating it or deactivating it
+     * accordingly.
+     *
+     * @return whether the mealType is active or not
+     */
+    public boolean toogleState() {
+
+        this.active = !this.active;
+        return isActive();
+    }
+
+    /**
+     * Change DishType description
+     *
+     * @param newDescription
+     *            New description.
+     */
+    public void changeDescriptionTo(String newDescription) {
+        if (!descriptionMeetsMinimumRequirements(newDescription)) {
+            throw new IllegalArgumentException();
+        }
+        this.description = newDescription;
+    }
+
+    @Override
+    public boolean is(String id) {
+        return id.equalsIgnoreCase(this.acronym);
+    }
+
+    @Override
+    public String id() {
+        return this.acronym;
+    }
+
+    @Override
+    public boolean sameAs(Object other) {
+        final MealType mealType = (MealType) other;
+        return this.equals(mealType) && description().equals(mealType.description())
+                && isActive() == mealType.isActive();
+    }
+
+    @Override
+    public int hashCode() {
+        return this.acronym.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof MealType)) {
+            return false;
+        }
+
+        final MealType other = (MealType) o;
+        return id().equals(other.id());
+    }
+
+    public DishTypeDTO toDTO() {
+        return new DishTypeDTO(acronym, description, active);
+    }
+}
