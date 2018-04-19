886acdf337f302940f62196418dd932ab282d426
Ruben Ribeiro (1160998) <1160998@isep.ipp.pt>
2018-04-19 17:20:50 +0100

    Criação de MealRatingUI, MealRatingController e MealRatingAction

---
+++ ecafeteria.app.user.console/src/main/java/eapli/ecafeteria/app/user/console/presentation/booking/MealRatingUI.java
@@ -0,0 +1,17 @@
+package eapli.ecafeteria.app.user.console.presentation.booking;
+
+//import eapli.ecafeteria.application.booking.MealRatingController;
+
+/**
+ *
+ * @author ruben
+ */
+public class MealRatingUI {
+    
+    //private final MealRatingController controller = new MealRatingController();
+
+    boolean show() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
