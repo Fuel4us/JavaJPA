1ca9aeb599f3f34c8374485df8369035c5c39e59
Pedro Monteiro - 1160630 <1160630@isep.ipp.pt>
2018-04-19 17:16:27 +0100

    Added ReasonBootstrapper Class;

---
+++ ecafeteria.bootstrappers/src/main/java/eapli/ecafeteria/bootstrapers/ReasonBootstrapper.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eapli.ecafeteria.bootstrapers;
+
+import eapli.framework.actions.Action;
+
+/**
+ *
+ * @author pedromonteiro
+ */
+public class ReasonBootstrapper implements Action{
+
+    public ReasonBootstrapper(){
+    }
+
+    @Override
+    public boolean execute() {
+        
+    }
+    
+    
+    
+}
