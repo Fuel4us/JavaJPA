d753e331e4a5eb684f34e4115c3b91d0425deaa9
Tiago João Santos Rios <1161292@isep.ipp.pt>
2018-04-19 12:39:40 +0100

    Criação das classes referentes às heurísticas

---
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/domain/meals/Heuristic.java
@@ -0,0 +1,11 @@
+package eapli.ecafeteria.domain.meals;
+
+/**
+ *
+ * @author Tiago João Santos Rios, 1161292@isep.ipp.pt
+ */
+public interface Heuristic {
+    
+    public void doHeuristicLogic();
+    
+}
