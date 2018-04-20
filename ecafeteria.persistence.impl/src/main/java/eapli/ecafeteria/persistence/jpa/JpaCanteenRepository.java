f3b3b1f477193d6d0e93e3ef0977c98b2cbf054a
Pedro Vieira (1160634) <1160634@isep.ipp.pt>
2018-04-20 15:01:46 +0100

    uc04 cozinha

--- ecafeteria.persistence.impl/src/main/java/eapli/ecafeteria/persistence/jpa/JpaCanteenRepository.java
+++ ecafeteria.persistence.impl/src/main/java/eapli/ecafeteria/persistence/jpa/JpaCanteenRepository.java
@@ -3,15 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package eapli.ecafeteria.persistence;
+package eapli.ecafeteria.persistence.jpa;
 
 import eapli.framework.domain.Designation;
-import eapli.framework.persistence.repositories.DataRepository;
 
 /**
  *
  * @author pedro
  */
-//public interface CanteenShiftRepository extends DataRepository<CanteenShift, Designation>{
+//public class JpaCanteenRepository extends CafeteriaJpaRepositoryBase<Canteen, Designation> implements CanteenRepository{
 //    
 //}
