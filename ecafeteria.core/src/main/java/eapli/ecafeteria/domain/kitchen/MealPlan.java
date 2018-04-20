3e5f7600f3e1a6b60cb8b441d97594030e57c1d4
Gonçalo Fonseca (1150503) <1150503@isep.ipp.pt>
2018-04-20 13:36:33 +0100

    [Implementation] Changes in MealPlan, added DB taggs also

--- ecafeteria.core/src/main/java/eapli/ecafeteria/domain/kitchen/MealPlan.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/domain/kitchen/MealPlan.java
@@ -2,31 +2,35 @@ package eapli.ecafeteria.domain.kitchen;
 
 import eapli.ecafeteria.domain.meals.Meal;
 
-import java.util.Date;
+import javax.persistence.*;
 
 /**
  * @author Gonçalo Fonseca <1150503@isep.ipp.pt>
  */
+@Entity
 public class MealPlan {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private Meal meal;
-    private Date initialDate;
-    private Date finalDate;
 
-    public MealPlan(Meal meal, Date initialDate, Date finalDate) {
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private Meal meal;
+    private int dishNumber;
+
+    public MealPlan(Meal meal, int dishNumber) {
         this.meal = meal;
-        this.initialDate = initialDate;
-        this.finalDate = finalDate;
+        this.dishNumber = dishNumber;
     }
+
+    protected MealPlan() {}
 
     @Override
     public String toString() {
         return "MealPlan{" +
                 "id=" + id +
                 ", meal=" + meal +
-                ", initialDate=" + initialDate +
-                ", finalDate=" + finalDate +
+                ", dishnumber=" + dishNumber +
                 '}';
     }
 }
