904abc6e637a597bb8e22246b52d5071444af3e8
Pedro Monteiro - 1160630 <1160630@isep.ipp.pt>
2018-04-25 19:15:23 +0100

    DeactivateUserUI now uses Selector Widget;

---
+++ ecafeteria.app.backoffice.console/src/main/java/eapli/ecafeteria/app/backoffice/console/presentation/authz/CafetariaUserPrinter.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eapli.ecafeteria.app.backoffice.console.presentation.authz;
+
+import eapli.ecafeteria.domain.authz.SystemUser;
+import eapli.ecafeteria.domain.cafeteriauser.CafeteriaUser;
+import eapli.framework.visitor.Visitor;
+
+/**
+ *
+ * @author pedromonteiro
+ */
+public class CafetariaUserPrinter implements Visitor<CafeteriaUser>{
+
+    @Override
+    public void visit(CafeteriaUser visitee) {
+        System.out.printf("%-10s%-30s%-30s%n", visitee.user().username(),
+                        visitee.user().name().firstName(), visitee.user().name().lastName());
+    }
+    
+}
