7d3b9e0a489b742889b710debc0f143cf3082789
Tiago João Santos Rios <1161292@isep.ipp.pt>
2018-04-25 15:01:33 +0100

    Adição das tags referentes à persistência dos objetos

--- ecafeteria.core/src/main/java/eapli/ecafeteria/domain/meals/HeuristicConfiguration.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/domain/meals/HeuristicConfiguration.java
@@ -2,13 +2,24 @@ package eapli.ecafeteria.domain.meals;
 
 import java.util.LinkedList;
 import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 /**
  *
  * @author Tiago João Santos Rios, 1161292@isep.ipp.pt
  */
+@Entity
 public class HeuristicConfiguration {
     
+    @Id
+    @GeneratedValue
+    private Long id;
+    
+    @OneToMany(cascade = CascadeType.ALL)
     private List<Heuristic> heuristicsAvailable;
     
     public HeuristicConfiguration() {
