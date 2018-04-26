bf373098be4fdbd75c9920524a7f71ed96980455
Hernani Gil(1050475) <1050475@isep.ipp.pt>
2018-04-26 23:13:23 +0100

    4.3.3 ChargeCardController
    ChargeCardUI doShow impl
    ChargeCardController cafeteriaUser findbyUsername
    MecanograficNumber number() to acess the number

--- ecafeteria.app.pos.console/src/main/java/eapli/ecafetaria/app/pos/console/presentation/finance/ChargeCardUI.java
+++ ecafeteria.app.pos.console/src/main/java/eapli/ecafetaria/app/pos/console/presentation/finance/ChargeCardUI.java
@@ -6,7 +6,16 @@
 package eapli.ecafetaria.app.pos.console.presentation.finance;
 
 import eapli.ecafetaria.application.finance.ChargeCardController;
+import eapli.ecafeteria.domain.authz.Username;
+import eapli.ecafeteria.domain.cafeteriauser.CafeteriaUser;
+import eapli.framework.persistence.DataConcurrencyException;
+import eapli.framework.persistence.DataIntegrityViolationException;
 import eapli.framework.presentation.console.AbstractUI;
+import eapli.framework.util.Console;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,15 +24,49 @@ import eapli.framework.presentation.console.AbstractUI;
 public class ChargeCardUI extends AbstractUI {
 
     private final ChargeCardController chargeCardController = new ChargeCardController();
-    
+
     @Override
     protected boolean doShow() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//        final List<CafeteriaUser> list = new ArrayList<>();
+        //final Iterable<CafeteriaUser> iterableCafeteriaUsers = this.chargeCardController.activeUsers();
+
+//        if (!iterableCafeteriaUsers.iterator().hasNext()) {
+//            System.out.println("There is no registered CafeteriaUser");
+//        }else{        
+//            int count = 1;
+//            System.out.printf("%-6s%-10s%-30s%-30s\n", "Nº:", "Username", "Firstname", "Lastname");
+//            for (final CafeteriaUser cafeteriaUser : iterableCafeteriaUsers) {
+//                list.add(cafeteriaUser);
+//                System.out.printf("Option: " + count +" Nº: " + cafeteriaUser.mecanographicNumber().number() + " Nome: " +  cafeteriaUser.user().name().firstName() + " " + cafeteriaUser.user().name().lastName());
+//                count++;
+//            }
+//            
+//            final int option = Console.readInteger("Enter the number corresponding to the user that you wish to add credits to or press 0 to deactivate.");
+//            if (option == 0) {
+//                System.out.println("No user selected");
+//            } else {
+//                CafeteriaUser selectedUser = list.get(option - 1);
+        boolean flag = false;
+        final String input = Console.readLine("ReadCard simulation: insert Cafeteria Username");
+
+        Username username = new Username(input);
+        flag = chargeCardController.selectUser(username);
+
+        if (flag) {
+            double moneyAmount = Console.readDouble("Enter the amount of money that you wish to add to to user " + input);
+            try {
+                chargeCardController.ChargeCard(moneyAmount);
+            } catch (DataConcurrencyException | DataIntegrityViolationException ex) {
+                Logger.getLogger(ChargeCardUI.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else {
+            return false;
+        }
+        return true;
     }
 
     @Override
-    public String headline() {
+        public String headline() {
         return "Charge Card";
     }
-    
 }
