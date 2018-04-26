5ee4c4276a0fb89b9ed3eb659487400deda46b76
Ana Silva (1161611) <1161611@isep.ipp.pt>
2018-04-26 16:17:48 +0100

    Alteração na UI e atualização do MainMenu

--- ecafeteria.app.pos.console/src/main/java/eapli/ecafeteria/app/pos/console/presentation/booking/BookingPrinter.java
+++ ecafeteria.app.pos.console/src/main/java/eapli/ecafeteria/app/pos/console/presentation/booking/BookingPrinter.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package eapli.ecafeteria.app.backoffice.console.presentation.booking;
+package eapli.ecafeteria.app.pos.console.presentation.booking;
 
 import eapli.ecafeteria.domain.booking.Booking;
 import eapli.framework.visitor.Visitor;
