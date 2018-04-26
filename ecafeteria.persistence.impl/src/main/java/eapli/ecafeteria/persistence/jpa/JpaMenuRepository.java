1274076a79680695e767c1b486d568c65d2e1a60
Joao Reis (1160600) <1160600@isep.ipp.pt>
2018-04-26 17:57:22 +0100

    findByMenuPeriod in jpamenuRepository implemented

--- ecafeteria.persistence.impl/src/main/java/eapli/ecafeteria/persistence/jpa/JpaMenuRepository.java
+++ ecafeteria.persistence.impl/src/main/java/eapli/ecafeteria/persistence/jpa/JpaMenuRepository.java
@@ -8,21 +8,35 @@ package eapli.ecafeteria.persistence.jpa;
 import eapli.ecafeteria.domain.menus.Menu;
 import eapli.ecafeteria.persistence.MenuRepository;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
  * @author joao reis (1160600)
  */
-public class JpaMenuRepository extends CafeteriaJpaRepositoryBase<Menu, Long> implements MenuRepository{
+public class JpaMenuRepository extends CafeteriaJpaRepositoryBase<Menu, Long> implements MenuRepository {
 
     @Override
     public Iterable<Menu> findByState(boolean state) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * Joao Reis -1160600 (not tested)
+     *
+     * @param beginning beginning of the time period
+     * @param end end of the time period
+     * @return all menus in repository that either start or end in the defined
+     * menu period
+     */
     @Override
     public Iterable<Menu> findByMenuPeriod(Date beginning, Date end) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        final Map<String, Object> params = new HashMap<>();
+        params.put("dateBeg", beginning);
+        params.put("dateEnd", beginning);
+
+        return match("(e.startDate >= :dateBeg and e.startDate<= :dateEnd) or (e.endDate >= :dateBeg and e.endDate<= :dateEnd)", params);
     }
-    
+
 }
