267fcd0e0af0e7289685c1c268676faf867be7fe
Hernani Gil(1050475) <1050475@isep.ipp.pt>
2018-04-26 23:55:14 +0100

    Muralha da China

--- ecafeteria.core/src/test/java/eapli/ecafeteria/domain/booking/BookingStateTest.java
+++ ecafeteria.core/src/test/java/eapli/ecafeteria/domain/booking/BookingStateTest.java
@@ -40,41 +40,41 @@ public class BookingStateTest {
     /**
      * Test of values method, of class BookingState.
      */
-    @Test
-    public void testValues() {
-        System.out.println("values");
-        BookingState[] expResult = null;
-        BookingState[] result = BookingState.values();
-        assertArrayEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of valueOf method, of class BookingState.
-     */
-    @Test
-    public void testValueOf() {
-        System.out.println("valueOf");
-        String name = "";
-        BookingState expResult = null;
-        BookingState result = BookingState.valueOf(name);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of BookingStateValues method, of class BookingState.
-     */
-    @Test
-    public void testBookingStateValues() {
-        System.out.println("BookingStateValues");
-        BookingState[] expResult = null;
-        BookingState[] result = BookingState.BookingStateValues();
-        assertArrayEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+//    @Test
+//    public void testValues() {
+//        System.out.println("values");
+//        BookingState[] expResult = null;
+//        BookingState[] result = BookingState.values();
+//        assertArrayEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of valueOf method, of class BookingState.
+//     */
+//    @Test
+//    public void testValueOf() {
+//        System.out.println("valueOf");
+//        String name = "";
+//        BookingState expResult = null;
+//        BookingState result = BookingState.valueOf(name);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of BookingStateValues method, of class BookingState.
+//     */
+//    @Test
+//    public void testBookingStateValues() {
+//        System.out.println("BookingStateValues");
+//        BookingState[] expResult = null;
+//        BookingState[] result = BookingState.BookingStateValues();
+//        assertArrayEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
     
 }
