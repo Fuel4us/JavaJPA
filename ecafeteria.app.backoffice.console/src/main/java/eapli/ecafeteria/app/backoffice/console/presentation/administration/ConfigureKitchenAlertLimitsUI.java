1299c414fff4345ba1b182c6f10d52798c1f9b64
1160629 <1160629@isep.ipp.pt>
2018-04-26 17:37:21 +0100

    Added empty classes for UC4.5.7 implementation. (issue #2)

---
+++ ecafeteria.app.backoffice.console/src/main/java/eapli/ecafeteria/app/backoffice/console/presentation/administration/ConfigureKitchenAlertLimitsUI.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eapli.ecafeteria.app.backoffice.console.presentation.administration;
+
+/**
+ *
+ * @author 1160629@isep.ipp.pt
+ */
+public class ConfigureKitchenAlertLimitsUI {
+    
+}
