11360d035b6928a72af8e1713c6c039676a68938
Diogo Monteiro (1140302) <1140302@isep.ipp.pt>
2018-04-26 12:57:22 +0100

    [Implementation] Method displayBookingsByDish() added, this method displays non-repeated dishes from all bookings and then asks to select one of them and displays the respective bookings Method displayBookingsDate() updated so that it handles the selection of a date and displays the respective bookings

--- ecafeteria.core/src/main/java/eapli/ecafeteria/application/kitchen/CheckBookingsService.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/application/kitchen/CheckBookingsService.java
@@ -1,8 +1,10 @@
 package eapli.ecafeteria.application.kitchen;
 
 import eapli.ecafeteria.domain.booking.Booking;
+import eapli.ecafeteria.domain.dishes.Dish;
 import eapli.ecafeteria.persistence.BookingRepository;
 import eapli.ecafeteria.persistence.PersistenceContext;
+import eapli.framework.util.Console;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -31,20 +33,64 @@ public class CheckBookingsService {
         return list;
     }
 
-    public void displayBookingsDate(){
+    public void displayBookingsDate() {
         List<Booking> list = getAllReservations();
         List<Date> dateList = new ArrayList<>();
 
-        if(list.size()>0){
-            for (Booking booking:
+        if (list.size() > 0) {
+            for (Booking booking :
                     list) {
-                if(!dateList.contains(booking.day())){
+                if (!dateList.contains(booking.day())) {
                     dateList.add(booking.day());
                 }
             }
-            for (int i = 0; i <dateList.size() ; i++) {
-                System.out.println(i+1+" Date: "+dateList.get(i));
+            for (int i = 0; i < dateList.size(); i++) {
+                System.out.println(i + 1 + " Booking Date: " + dateList.get(i) + "\n");
             }
+
+            int choice = Console.readInteger("Which date do you want to see the respective Bookings?\n");
+            if (choice > 0 && choice < dateList.size() + 1){
+                for (Booking booking :
+                        list) {
+                    if (booking.day().equals(dateList.get(choice - 1))) {
+                        System.out.println(booking);
+                    }
+                }
+        }else{
+                System.out.println("That option doesn't exist");
+                displayBookingsDate();
+            }
+    }
+    }
+
+    public void displayBookingsByDish(){
+        List<Booking> list = getAllReservations();
+        List<Dish> dishList = new ArrayList<>();
+
+        if(list.size()>0){
+            for (Booking booking:
+                 list) {
+                if(!dishList.contains(booking.getMeal().getDish())){
+                    dishList.add(booking.getMeal().getDish());
+                }
+            }
+        }
+
+        for (int i = 0; i < dishList.size(); i++) {
+            System.out.printf(i+1+" Booking Dish: "+dishList.get(i)+"\n");
+        }
+
+        int choice = Console.readInteger("Which date do you want to see the respective Bookings?\n");
+        if(choice>0&&choice<dishList.size()+1){
+            for (Booking booking:
+                 list) {
+                if(booking.getMeal().getDish().equals(dishList.get(choice-1))){
+                    System.out.println(booking);
+                }
+            }
+        }else{
+            System.out.println("That option doesn't exist");
+            displayBookingsByDish();
         }
     }
 }
