bc66190787b3417e33a64a4d4c986d9a9147538f
Joao Reis (1160600) <1160600@isep.ipp.pt>
2018-04-26 17:21:36 +0100

    uc 4.1.4 implementation getMenuForCurrentWeek and getMenuForNextWeek methods

--- ecafeteria.core/src/main/java/eapli/ecafeteria/application/menus/ListMenuService.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/application/menus/ListMenuService.java
@@ -12,8 +12,10 @@ import eapli.ecafeteria.domain.menus.Menu;
 import eapli.ecafeteria.persistence.MenuRepository;
 import eapli.ecafeteria.persistence.PersistenceContext;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 /**
@@ -39,5 +41,38 @@ public class ListMenuService {
         
         Collections.sort(mealList, Meal.compareDates());
         return mealList;
+    }
+    
+    public List<Meal> menuForCurrentWeek(){
+        Date beginning, end, aux;
+        aux = new Date();
+        
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(aux);
+        while(cal.get(Calendar.DAY_OF_WEEK)> cal.getFirstDayOfWeek()) {
+            cal.add(Calendar.DATE, -1);
+        }
+        beginning = cal.getTime();
+        cal.add(Calendar.DATE, 7);
+        end = cal.getTime();
+        
+        return menuByPeriod(beginning, end);
+    }
+    
+    public List<Meal> menuForNextWeek() {
+         Date beginning, end, aux;
+        aux = new Date();
+        
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(aux);
+        while(cal.get(Calendar.DAY_OF_WEEK)> cal.getFirstDayOfWeek()) {
+            cal.add(Calendar.DATE, -1);
+        }
+        cal.add(Calendar.DATE, 8);
+        beginning = cal.getTime();
+        cal.add(Calendar.DATE, 7);
+        end = cal.getTime();
+        
+        return menuByPeriod(beginning, end);
     }
 }
