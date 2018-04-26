91bc995b9e27fe35e367ac902c83a6e981e18809
Gonçalo Fonseca (1150503) <1150503@isep.ipp.pt>
2018-04-26 23:56:34 +0100

    [Implementation] MealPlanTest Created

--- ecafeteria.core/src/test/java/eapli/ecafeteria/domain/kitchen/MealPlanTest.java
+++ ecafeteria.core/src/test/java/eapli/ecafeteria/domain/kitchen/MealPlanTest.java
@@ -6,6 +6,9 @@
 package eapli.ecafeteria.domain.kitchen;
 
 import eapli.ecafeteria.domain.menus.Menu;
+
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -17,6 +20,12 @@ import static org.junit.Assert.*;
  * @author joaotiagow
  */
 public class MealPlanTest {
+
+    private Menu menu = new Menu(new Date(), new Date());
+    private List<Integer> dishes = new ArrayList<>();
+
+    private MealPlan meal = new MealPlan(menu, dishes);
+    private MealPlan meal2 = new MealPlan(menu, dishes);
     
     public MealPlanTest() {
     }
@@ -49,6 +58,15 @@ public class MealPlanTest {
         boolean expResult = true;
         boolean result = MealPlan.changeHeuristicInUse(newHeuristic);
         
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void changeState() {
+        meal.changeState(meal);
+        boolean expResult = true;
+        boolean result = meal.isClosed();
+
         assertEquals(expResult, result);
     }
 }
