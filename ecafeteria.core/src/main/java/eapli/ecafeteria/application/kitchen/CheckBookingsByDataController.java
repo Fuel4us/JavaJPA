77eb5b106c212183c2386a1bb07f724059d80363
Diogo Monteiro (1140302) <1140302@isep.ipp.pt>
2018-04-27 10:15:02 +0100

    [Implementation] Added user authorization

--- ecafeteria.core/src/main/java/eapli/ecafeteria/application/kitchen/CheckBookingsByDataController.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/application/kitchen/CheckBookingsByDataController.java
@@ -1,5 +1,7 @@
 package eapli.ecafeteria.application.kitchen;
 
+import eapli.ecafeteria.application.authz.AuthorizationService;
+import eapli.ecafeteria.domain.authz.ActionRight;
 import eapli.ecafeteria.domain.booking.Booking;
 import eapli.ecafeteria.domain.dishes.Dish;
 import eapli.ecafeteria.domain.meals.Meal;
@@ -24,24 +26,29 @@ public class CheckBookingsByDataController implements Controller {
 
 
     public List<Booking> getAllBookings(){
+        AuthorizationService.ensurePermissionOfLoggedInUser(ActionRight.MANAGE_KITCHEN);
         return service.getAllReservations();
     }
 
 
     public List<Date> displayBookingsDate() {
+        AuthorizationService.ensurePermissionOfLoggedInUser(ActionRight.MANAGE_KITCHEN);
         return service.displayBookingsDate();
 
     }
 
     public List<Dish> displayBookingsByDish(){
+        AuthorizationService.ensurePermissionOfLoggedInUser(ActionRight.MANAGE_KITCHEN);
         return service.displayBookingsByDish();
     }
 
     public List<Meal> displayBookingsByMeal(){
+        AuthorizationService.ensurePermissionOfLoggedInUser(ActionRight.MANAGE_KITCHEN);
         return service.displayBookingsByMeal();
     }
 
     public List<MealType> displayBookingsByMealType(){
+        AuthorizationService.ensurePermissionOfLoggedInUser(ActionRight.MANAGE_KITCHEN);
         return service.displayBookingsByMealType();
     }
 }
