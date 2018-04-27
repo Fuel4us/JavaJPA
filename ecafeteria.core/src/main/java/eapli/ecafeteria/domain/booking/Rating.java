22b4b3837ffb697a2efdd2625f3f658dbd2f6fb8
Hilario Coelho 1160557 <1160557@isep.ipp.pt>
2018-04-27 10:15:41 +0100

    Create rating (update booking) fix

--- ecafeteria.core/src/main/java/eapli/ecafeteria/domain/booking/Rating.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/domain/booking/Rating.java
@@ -7,6 +7,7 @@ package eapli.ecafeteria.domain.booking;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 /**
@@ -17,7 +18,7 @@ import javax.persistence.Id;
 public class Rating {
      
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     
     private int score;
@@ -41,5 +42,10 @@ public class Rating {
 
     public void setComment(String comment) {
         this.comment = comment;
+    }
+
+    @Override
+    public String toString() {
+        return "Rating{" + "id=" + id + ", score=" + score + ", comment=" + comment + '}';
     }
 }
