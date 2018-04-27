51faac3a00220e09da3593969a14c0fdfda3fa7e
Gonçalo Silva (1161140) <1161140@isep.ipp.pt>
2018-04-27 10:27:35 +0100

    Removed unused repositories

--- ecafeteria.persistence.impl/src/main/java/eapli/ecafeteria/persistence/inmemory/InMemoryLotRepository.java
+++
@@ -1,9 +0,0 @@
-package eapli.ecafeteria.persistence.inmemory;
-
-import eapli.ecafeteria.domain.kitchen.Lot;
-import eapli.framework.persistence.repositories.impl.inmemory.InMemoryRepositoryWithLongPK;
-import eapli.ecafeteria.persistence.LotRepository;
-
-public class InMemoryLotRepository extends InMemoryRepositoryWithLongPK<Lot> implements LotRepository {
-
-}
