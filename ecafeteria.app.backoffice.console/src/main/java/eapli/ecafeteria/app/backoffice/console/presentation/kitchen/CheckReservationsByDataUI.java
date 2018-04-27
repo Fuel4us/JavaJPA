3600d206b43b3102bd568218a9ca2c14d72ad17d
Diogo Monteiro (1140302) <1140302@isep.ipp.pt>
2018-04-27 10:00:35 +0100

    [Implementation] Changed the organization of the classes used for UC 4.2.2 ( doesn't change how the program works)

--- ecafeteria.app.backoffice.console/src/main/java/eapli/ecafeteria/app/backoffice/console/presentation/kitchen/CheckReservationsByDataUI.java
+++ ecafeteria.app.backoffice.console/src/main/java/eapli/ecafeteria/app/backoffice/console/presentation/kitchen/CheckReservationsByDataUI.java
@@ -1,8 +1,15 @@
 package eapli.ecafeteria.app.backoffice.console.presentation.kitchen;
 
 import eapli.ecafeteria.application.kitchen.CheckBookingsByDataController;
+import eapli.ecafeteria.domain.booking.Booking;
+import eapli.ecafeteria.domain.dishes.Dish;
+import eapli.ecafeteria.domain.meals.Meal;
+import eapli.ecafeteria.domain.meals.MealType;
 import eapli.framework.presentation.console.AbstractUI;
 import eapli.framework.util.Console;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  *
@@ -23,5 +30,76 @@ public class CheckReservationsByDataUI extends AbstractUI {
     @Override
     public String headline() {
         return "Check Reservations By Data";
+    }
+
+    public void displayBookingsByDate(){
+        List<Booking> list = controller.getAllBookings();
+        List<Date> dateList = controller.displayBookingsDate();
+        for (int i = 0; i < dateList.size(); i++) {
+            System.out.println(i + 1 + " Booking Date: " + dateList.get(i) + "\n");
+        }
+
+        int choice = Console.readInteger("Which date do you want to see the respective Bookings?\n");
+        if (choice > 0 && choice < dateList.size() + 1) {
+            for (Booking booking :
+                    list) {
+                if (booking.day().equals(dateList.get(choice - 1))) {
+                    System.out.println(booking + "\n");
+                }
+            }
+        }
+    }
+
+    public void diplayBookingsByDish(){
+        List<Booking> list = controller.getAllBookings();
+        List<Dish> dishList = controller.displayBookingsByDish();
+        for (int i = 0; i < dishList.size(); i++) {
+            System.out.println(i + 1 + " Booking Dish: " + dishList.get(i) + "\n");
+        }
+
+        int choice = Console.readInteger("Which Dish do you want to see the respective Bookings?\n");
+        if(choice>0&&choice<dishList.size()+1){
+            for (Booking booking:
+                    list) {
+                if(booking.getMeal().getDish().equals(dishList.get(choice-1))){
+                    System.out.println(booking+"\n");
+                }
+            }
+        }
+    }
+
+    public void displayBookingsByMeal(){
+        List<Booking> list = controller.getAllBookings();
+        List<Meal> mealList = controller.displayBookingsByMeal();
+        for (int i = 0; i < mealList.size(); i++) {
+            System.out.println(i + 1 + " Booking Meal: " + mealList.get(i) + "\n");
+        }
+
+        int choice = Console.readInteger("Which Meal do you want to see the respective Bookings?\n");
+        if(choice>0&&choice<mealList.size()+1){
+            for (Booking booking:
+                    list) {
+                if(booking.getMeal().equals(mealList.get(choice-1))){
+                    System.out.println(booking+"\n");
+                }
+            }
+        }
+    }
+
+    public void displayBookingsByMealType(){
+        List<Booking> list = controller.getAllBookings();
+        List<MealType> mealTypeList = controller.displayBookingsByMealType();
+        for (int i = 0; i < mealTypeList.size(); i++) {
+            System.out.println(i + 1 + " Booking Meal Type: " + mealTypeList.get(i) + "\n");
+        }
+        int choice = Console.readInteger("Which Meal Type do you want to see the respective Bookings?\n");
+        if(choice>0&&choice<mealTypeList.size()+1){
+            for (Booking booking:
+                    list) {
+                if(booking.getMeal().equals(mealTypeList.get(choice-1))){
+                    System.out.println(booking+"\n");
+                }
+            }
+        }
     }
 }
--- ecafeteria.core/src/main/java/eapli/ecafeteria/application/kitchen/CheckBookingsByDataController.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/application/kitchen/CheckBookingsByDataController.java
@@ -1,6 +1,14 @@
 package eapli.ecafeteria.application.kitchen;
 
+import eapli.ecafeteria.domain.booking.Booking;
+import eapli.ecafeteria.domain.dishes.Dish;
+import eapli.ecafeteria.domain.meals.Meal;
+import eapli.ecafeteria.domain.meals.MealType;
 import eapli.framework.application.Controller;
+import eapli.framework.util.Console;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  *
@@ -25,4 +33,25 @@ public class CheckBookingsByDataController implements Controller {
         }
     }
 
+    public List<Booking> getAllBookings(){
+    return service.getAllReservations();
+    }
+
+
+    public List<Date> displayBookingsDate() {
+        return service.displayBookingsDate();
+
+    }
+
+    public List<Dish> displayBookingsByDish(){
+        return service.displayBookingsByDish();
+    }
+
+    public List<Meal> displayBookingsByMeal(){
+        return service.displayBookingsByMeal();
+    }
+
+    public List<MealType> displayBookingsByMealType(){
+        return service.displayBookingsByMealType();
+    }
 }
