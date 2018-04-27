66959a0c2056e67cecf04608866085f4ef18db2d
Gonçalo Silva (1161140) <1161140@isep.ipp.pt>
2018-04-27 10:29:10 +0100

    Updated Lot and MealLot repositories

--- ecafeteria.core/src/main/java/eapli/ecafeteria/persistence/LotRepository.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/persistence/LotRepository.java
@@ -3,6 +3,10 @@ package eapli.ecafeteria.persistence;
 import eapli.ecafeteria.domain.kitchen.Lot;
 import eapli.framework.persistence.repositories.DataRepository;
 
+/**
+ * @author Gonçalo Silva (1161140)
+ */
 public interface LotRepository extends DataRepository<Lot, Long> {
-
+    
+    Long findPkWithCode(int lotCode);
 }
--- ecafeteria.persistence.impl/src/main/java/eapli/ecafeteria/persistence/jpa/JpaLotRepository.java
+++ ecafeteria.persistence.impl/src/main/java/eapli/ecafeteria/persistence/jpa/JpaLotRepository.java
@@ -2,7 +2,23 @@ package eapli.ecafeteria.persistence.jpa;
 
 import eapli.ecafeteria.domain.kitchen.Lot;
 import eapli.ecafeteria.persistence.LotRepository;
+import java.util.HashMap;
+import java.util.Map;
+import javax.persistence.Query;
 
+/**
+ * @author Gonçalo Silva (1161140)
+ */
 public class JpaLotRepository extends CafeteriaJpaRepositoryBase<Lot, Long> implements LotRepository {
 
+    @Override
+    public Long findPkWithCode(int lotCode) {
+        final Map<String, Object> params = new HashMap<>();
+        params.put("lot", lotCode);
+
+        Query query = entityManager().createQuery("select l.pk from " + this.entityClass.getSimpleName() + " l where l.lotCode = :lotCode");
+        query.setParameter("lotCode", lotCode);
+
+        return (Long) query.getSingleResult();
+    }
 }
