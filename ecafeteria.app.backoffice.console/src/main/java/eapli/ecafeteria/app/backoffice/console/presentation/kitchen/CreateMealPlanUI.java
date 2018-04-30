39aa169eea1df6cc9aadc7d9968bb5ce428cebd9
Tiago Babo (1160760) <1160760@isep.ipp.pt>
2018-04-30 09:40:10 +0100

    Continued implementation of UI and Controller

--- ecafeteria.app.backoffice.console/src/main/java/eapli/ecafeteria/app/backoffice/console/presentation/kitchen/CreateMealPlanUI.java
+++ ecafeteria.app.backoffice.console/src/main/java/eapli/ecafeteria/app/backoffice/console/presentation/kitchen/CreateMealPlanUI.java
@@ -2,6 +2,8 @@ package eapli.ecafeteria.app.backoffice.console.presentation.kitchen;
 
 import eapli.ecafeteria.application.kitchen.CreateMealPlanController;
 import eapli.ecafeteria.domain.kitchen.MealPlan;
+import eapli.ecafeteria.domain.kitchen.MealPlanItem;
+import eapli.ecafeteria.domain.kitchen.MealPlanItemQuantity;
 import eapli.ecafeteria.domain.meals.Meal;
 import eapli.ecafeteria.domain.menus.Menu;
 import eapli.framework.presentation.console.AbstractUI;
@@ -28,10 +30,8 @@ public class CreateMealPlanUI extends AbstractUI{
                     break;
                 case 1:
                     setDishQuantity();
+                    System.out.println("Meal plan saved successfully!");
                     break;
-//                case 2:
-//                    editMealPlan();
-//                    break;
                 default:
                     System.out.println("\nINVALID OPTION!");
                     break;
@@ -42,7 +42,6 @@ public class CreateMealPlanUI extends AbstractUI{
     public int menuUI(){
         System.out.println("");
         System.out.println("1. Create meal plan");
-//        System.out.println("2. Edit meal plan");
         System.out.println("0. Leave");
         System.out.printf("OPTION: ");
         int option = input.nextInt();
@@ -59,11 +58,11 @@ public class CreateMealPlanUI extends AbstractUI{
         int i = 0;
 
         for (Menu menu : menuList) {
-            System.out.println(i + ". Menu with starting date: " + menu.getStartDate()
-                                 + "and finishing date: " + menu.getEndDate());
+            System.out.println(i + ". Menu with starting date " + menu.getStartDate()
+                                 + " and finishing date " + menu.getEndDate());
             i++;
         }
-        System.out.printf("\nOPTION: ");
+        System.out.printf("OPTION: ");
         Integer opcao = input.nextInt();
 
         Menu selectedMenu = controller.getMenu(menuList, opcao);
@@ -75,11 +74,13 @@ public class CreateMealPlanUI extends AbstractUI{
         Menu selectedMenu = selectMenu();
         
         MealPlan mealPlan = controller.createMealPlan(selectedMenu);
+        MealPlanItem item = null;
+        MealPlanItemQuantity itemQuantity = null;
 
         System.out.println("\nAssign the number of dishes for each of the meals:");
 
         int i = 0;
-        Integer numberOfDishes;
+        int quantity;
 
         for (Meal meal : mealPlan.getMenu().getMealList()) {
             System.out.printf(i + " --> %s || %s || %s || %s\n",
@@ -88,10 +89,11 @@ public class CreateMealPlanUI extends AbstractUI{
                                                     controller.getMealDishName(meal),
                                                     controller.getMealType(meal));
             System.out.printf("Number of dishes: ");
-            numberOfDishes = input.nextInt();
+            quantity = input.nextInt();
             System.out.printf("\n");
 
-            controller.setDishQuantity(mealPlan, numberOfDishes);
+            item = controller.setPlanItem(meal, mealPlan);
+            itemQuantity = controller.setItemQuantity(meal, quantity, mealPlan, item);
 
             i++;
         }
@@ -109,59 +111,4 @@ public class CreateMealPlanUI extends AbstractUI{
     public String headline() {
         return "CREATE MEAL PLAN";
     }
-//    /*=======================================================================*/
-//    
-//    public MealPlan selectMealPlan() {
-//        List<MealPlan> mealPlanList = controller.getExistingMealPlan();
-//        
-//        System.out.println("\n## Select the meal plan you wish to edit: ##");
-//        
-//        for(MealPlan mp: mealPlanList){
-//            int i = 0;
-//            System.out.println(i + ". Meal Plan of Menu with starting date: " + mp.getMenu().getStartDate()
-//                                 + "and finishing date: " + mp.getMenu().getEndDate());
-//            i++;
-//        }
-//        System.out.printf("\nOPTION: ");
-//        Integer opcao = input.nextInt();
-//        
-//        MealPlan selectedMealPlan = controller.getMealPlan(mealPlanList, opcao);
-//        
-//        return selectedMealPlan;
-//    }
-//    
-//    public void editMealPlan(){
-//        MealPlan selectedMealPlan = selectMealPlan();
-//        
-//        System.out.println("\nAssign the new number of dishes for each of the meals:");
-//
-//        int i = 0;
-//        Integer numberOfDishes;
-//        
-//        List<Integer> oldNumberOfDishes = selectedMealPlan.getNumberOfDishes();
-//        
-//        controller.cleanNumberOfDishes(selectedMealPlan);
-//        
-//        for (Meal meal : selectedMealPlan.getMenu().getMealList()) {
-//            for(Integer numDish: oldNumberOfDishes){
-//                System.out.printf(i + " --> %s || %s || %s || %s\n",
-//                                                        controller.getMealDate(meal),
-//                                                        controller.getMealDishType(meal),
-//                                                        controller.getMealDishName(meal),
-//                                                        controller.getMealType(meal));
-//                System.out.printf("Old Number of Dishes -> %d\n", numDish);
-//                System.out.printf("New Number of dishes: ");
-//                numberOfDishes = input.nextInt();
-//                System.out.printf("\n");
-//
-//                controller.setDishQuantity(selectedMealPlan, numberOfDishes);
-//
-//                i++;
-//            }
-//        }
-//        
-//        controller.saveMealPlan(selectedMealPlan);
-//    }
-//    
-//    /*=======================================================================*/
 }
