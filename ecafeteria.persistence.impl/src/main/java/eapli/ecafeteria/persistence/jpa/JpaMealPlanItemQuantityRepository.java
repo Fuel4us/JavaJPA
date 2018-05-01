363a793bf7f7ba1cfc5df101e13f3617a57920ca
Tiago Babo (1160760) <1160760@isep.ipp.pt>
2018-05-01 18:06:40 +0100

    Created new repositories

---
+++ ecafeteria.persistence.impl/src/main/java/eapli/ecafeteria/persistence/jpa/JpaMealPlanItemQuantityRepository.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eapli.ecafeteria.persistence.jpa;
+
+import eapli.ecafeteria.domain.kitchen.MealPlanItemQuantity;
+import eapli.ecafeteria.persistence.MealPlanItemQuantityRepository;
+
+/**
+ *
+ * @author Tiago Babo 1160760
+ */
+public class JpaMealPlanItemQuantityRepository extends CafeteriaJpaRepositoryBase<MealPlanItemQuantity, Long> implements MealPlanItemQuantityRepository{
+    
+}
