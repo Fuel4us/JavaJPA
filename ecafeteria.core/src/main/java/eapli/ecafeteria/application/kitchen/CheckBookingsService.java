6d4601f31d26913143cb87b82e25920a53bda65f
Diogo Monteiro (1140302) <1140302@isep.ipp.pt>
2018-05-02 02:38:50 +0100

    [Implementation] Changed methods so that the new filter can be applied ( selecting which state the booking is in )

--- ecafeteria.core/src/main/java/eapli/ecafeteria/application/kitchen/CheckBookingsService.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/application/kitchen/CheckBookingsService.java
@@ -1,6 +1,7 @@
 package eapli.ecafeteria.application.kitchen;
 
 import eapli.ecafeteria.domain.booking.Booking;
+import eapli.ecafeteria.domain.booking.BookingState;
 import eapli.ecafeteria.domain.dishes.Dish;
 import eapli.ecafeteria.domain.meals.Meal;
 import eapli.ecafeteria.domain.meals.MealType;
@@ -24,7 +25,7 @@ public class CheckBookingsService {
 
     }
 
-    public List<Booking>  getAllReservations(){
+    public List<Booking>  getAllReservations(int state){
         Iterable<Booking> it = rep.findAll();
         List<Booking> list = new ArrayList<>();
         if(!it.iterator().hasNext()){
@@ -32,32 +33,38 @@ public class CheckBookingsService {
         }else{
             for (Booking booking:
                     it ) {
-                list.add(booking);
+                if(state==1&&booking.isReserved()){
+                    list.add(booking);
+                }else if(state==2&&booking.isDelivered()){
+                    list.add(booking);
+                }else if(state ==3&&booking.isCancelled()){
+                    list.add(booking);
+                }
+                else if(state==4) {
+
+                    list.add(booking);
+                }
             }
         }
 
         return list;
     }
 
-    public List<Date> displayBookingsDate() {
-        List<Booking> list = getAllReservations();
+    public List<Date> displayBookingsDate(List<Booking> list) {
         List<Date> dateList = new ArrayList<>();
-
-        if (list.size() > 0) {
-            for (Booking booking :
-                    list) {
-                if (!dateList.contains(booking.day())) {
-                    dateList.add(booking.day());
+            if (list.size() > 0) {
+                for (Booking booking :
+                        list) {
+                    if (!dateList.contains(booking.day())) {
+                        dateList.add(booking.day());
+                    }
                 }
             }
-        }
         return dateList;
     }
 
-    public List<Dish> displayBookingsByDish(){
-        List<Booking> list = getAllReservations();
+    public List<Dish> displayBookingsByDish(List<Booking> list){
         List<Dish> dishList = new ArrayList<>();
-
         if(list.size()>0){
             for (Booking booking:
                     list) {
@@ -70,10 +77,8 @@ public class CheckBookingsService {
         return dishList;
     }
 
-    public List<Meal> displayBookingsByMeal(){
-        List<Booking> list = getAllReservations();
+    public List<Meal> displayBookingsByMeal(List<Booking> list){
         List<Meal> mealList = new ArrayList<>();
-
         if(list.size()>0){
             for (Booking booking:
                     list) {
@@ -86,8 +91,7 @@ public class CheckBookingsService {
         return mealList;
     }
 
-    public List<MealType> displayBookingsByMealType(){
-        List<Booking> list = getAllReservations();
+    public List<MealType> displayBookingsByMealType(List<Booking> list){
         List<MealType> mealTypeList = new ArrayList<>();
         if(list.size()>0){
             for (Booking booking:
