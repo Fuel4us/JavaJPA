1b14ce02e281bbf87f931e755a21ce6dc2866aa7
Diogo Monteiro (1140302) <1140302@isep.ipp.pt>
2018-05-02 02:42:34 +0100

    [UI] User interface changed so that the person using it can choose which state of the bookings it wants to see. [Documentation] Use case 4.2.2 sucessfully done. Other funcionalities might be implemented in the future

--- ecafeteria.app.backoffice.console/src/main/java/eapli/ecafeteria/app/backoffice/console/presentation/kitchen/CheckReservationsByDataUI.java
+++ ecafeteria.app.backoffice.console/src/main/java/eapli/ecafeteria/app/backoffice/console/presentation/kitchen/CheckReservationsByDataUI.java
@@ -19,18 +19,29 @@ public class CheckReservationsByDataUI extends AbstractUI {
     CheckBookingsByDataController controller = new CheckBookingsByDataController();
     @Override
     protected boolean doShow() {
-
-        int choice = Console.readInteger("What information do you want to use for the search?\n1 - Day\n2 - Meal Type\n3- Dish\n4 - Meal");
-        if(choice>0&&choice<5){
-            if(choice==1){
-                displayBookingsByDate();
-            }else if(choice ==2){
-                displayBookingsByMealType();
-            }else if(choice ==3){
-                diplayBookingsByDish();
-            }else if(choice ==4){
-                displayBookingsByMeal();
+        int state = Console.readInteger("Select a filter for the bookings\n1 - Reserved\n2 - Delivered\n3 - Canceled\n4 - All of the above");
+        List<Booking> list = controller.getAllBookings(state);
+        if(!list.isEmpty()) {
+            controller.setBookingList(list);
+            if (state > 0 && state < 5) {
+                int choice = Console.readInteger("What information do you want to use for the search?\n1 - Day\n2 - Meal Type\n3 - Dish\n4 - Meal");
+                if (choice > 0 && choice < 5) {
+                    if (choice == 1) {
+                        displayBookingsByDate(list);
+                    } else if (choice == 2) {
+                        displayBookingsByMealType(list);
+                    } else if (choice == 3) {
+                        diplayBookingsByDish(list);
+                    } else if (choice == 4) {
+                        displayBookingsByMeal(list);
+                    }
+                }
+            } else {
+                System.out.println("There is no such choice");
+                ;
             }
+        }else{
+            System.out.println("There are no bookings for this filter");
         }
         return true;
     }
@@ -40,26 +51,34 @@ public class CheckReservationsByDataUI extends AbstractUI {
         return "Check Reservations By Data";
     }
 
-    public void displayBookingsByDate(){
-        List<Booking> list = controller.getAllBookings();
-        List<Date> dateList = controller.displayBookingsDate();
-        for (int i = 0; i < dateList.size(); i++) {
-            System.out.println(i + 1 + " Booking Date: " + dateList.get(i) + "\n");
-        }
+    public void displayBookingsByDate(List<Booking> list){
 
-        int choice = Console.readInteger("Which date do you want to see the respective Bookings?\n");
-        if (choice > 0 && choice < dateList.size() + 1) {
-            for (Booking booking :
-                    list) {
-                if (booking.day().equals(dateList.get(choice - 1))) {
-                    System.out.println(booking + "\n");
-                }
+        if(!list.isEmpty()) {
+            List<Date> dateList = controller.displayBookingsDate();
+            for (int i = 0; i < dateList.size(); i++) {
+                System.out.println(i + 1 + " Booking Date: " + dateList.get(i) + "\n");
             }
+
+            int choice = Console.readInteger("Which date do you want to see the respective Bookings?\n");
+            if (choice > 0 && choice < dateList.size() + 1) {
+                for (Booking booking :
+                        list) {
+                    if (booking.day().equals(dateList.get(choice - 1))) {
+                        System.out.println(booking);
+                    }
+                }
+            } else {
+                System.out.println("That option doesn't exist");
+                displayBookingsByDate(list);
+            }
+        }else{
+            System.out.println("There are no bookings for this filter");
         }
     }
 
-    public void diplayBookingsByDish(){
-        List<Booking> list = controller.getAllBookings();
+    public void diplayBookingsByDish(List<Booking> list){
+
+        if (!list.isEmpty()){
         List<Dish> dishList = controller.displayBookingsByDish();
         for (int i = 0; i < dishList.size(); i++) {
             System.out.println(i + 1 + " Booking Dish: " + dishList.get(i) + "\n");
@@ -70,14 +89,21 @@ public class CheckReservationsByDataUI extends AbstractUI {
             for (Booking booking:
                     list) {
                 if(booking.getMeal().getDish().equals(dishList.get(choice-1))){
-                    System.out.println(booking+"\n");
+                    System.out.println(booking);
                 }
             }
+        }else{
+            System.out.println("That option doesn't exist");
+            diplayBookingsByDish(list);
+        }
+        }else{
+            System.out.println("There are no bookings for this filter");
         }
     }
 
-    public void displayBookingsByMeal(){
-        List<Booking> list = controller.getAllBookings();
+    public void displayBookingsByMeal(List<Booking> list){
+
+        if(list.isEmpty()){
         List<Meal> mealList = controller.displayBookingsByMeal();
         for (int i = 0; i < mealList.size(); i++) {
             System.out.println(i + 1 + " Booking Meal: " + mealList.get(i) + "\n");
@@ -88,14 +114,20 @@ public class CheckReservationsByDataUI extends AbstractUI {
             for (Booking booking:
                     list) {
                 if(booking.getMeal().equals(mealList.get(choice-1))){
-                    System.out.println(booking+"\n");
+                    System.out.println(booking);
                 }
             }
+        }else{
+            System.out.println("That option doesn't exist");
+            displayBookingsByMeal(list);
+        }
+        }else{
+            System.out.println("There are no bookings for this filter");
         }
     }
 
-    public void displayBookingsByMealType(){
-        List<Booking> list = controller.getAllBookings();
+    public void displayBookingsByMealType(List<Booking> list){
+        if(!list.isEmpty()){
         List<MealType> mealTypeList = controller.displayBookingsByMealType();
         for (int i = 0; i < mealTypeList.size(); i++) {
             System.out.println(i + 1 + " Booking Meal Type: " + mealTypeList.get(i) + "\n");
@@ -105,12 +137,15 @@ public class CheckReservationsByDataUI extends AbstractUI {
             for (Booking booking:
                     list) {
                 if(booking.getMeal().getMealType().equals(mealTypeList.get(choice-1))){
-                    System.out.println(booking+"\n");
+                    System.out.println(booking);
                 }
             }
         }else{
             System.out.println("That option doesn't exist");
-            displayBookingsByMealType();
+            displayBookingsByMealType(list);
         }
+    }else{
+        System.out.println("There are no bookings for this filter");
+    }
     }
 }
