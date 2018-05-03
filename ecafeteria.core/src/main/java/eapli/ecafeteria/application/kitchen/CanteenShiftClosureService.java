c043933199268ceb64a90f8a62bcf2f179b72dbb
Diogo Monteiro (1140302) <1140302@isep.ipp.pt>
2018-05-03 17:12:56 +0100

    [Implementation] Added atribute from class ExecutionRepository to CanteenShiftClosureService to have access to the Execution's present in the database Created CanteenShiftClosureService empty constructor to be used in the future in class CanteenShiftClosureController

--- ecafeteria.core/src/main/java/eapli/ecafeteria/application/kitchen/CanteenShiftClosureService.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/application/kitchen/CanteenShiftClosureService.java
@@ -1,6 +1,15 @@
 package eapli.ecafeteria.application.kitchen;
 
+import eapli.ecafeteria.persistence.ExecutionRepository;
+import eapli.ecafeteria.persistence.PersistenceContext;
+
 public class CanteenShiftClosureService {
+
+    final ExecutionRepository repository= PersistenceContext.repositories().execution();
+
+    public CanteenShiftClosureService(){
+
+    }
 
 
 }
