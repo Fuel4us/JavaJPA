7d09b2c926d9f4561e16f3f1ac7cfe8ac301690b
Josué Lapa (1160713) <1160713@isep.ipp.pt>
2018-05-03 13:46:44 +0100

    4.3.1 - Small Correction
    Signed-off-by: Josué Lapa (1160713) <1160713@isep.ipp.pt>

--- ecafeteria.persistence.impl/src/main/java/eapli/ecafeteria/persistence/jpa/JpaPOSRepository.java
+++ ecafeteria.persistence.impl/src/main/java/eapli/ecafeteria/persistence/jpa/JpaPOSRepository.java
@@ -2,9 +2,13 @@ package eapli.ecafeteria.persistence.jpa;
 
 import eapli.ecafeteria.domain.finance.POS;
 import eapli.ecafeteria.persistence.POSRepository;
+import eapli.framework.persistence.DataConcurrencyException;
+import eapli.framework.persistence.DataIntegrityViolationException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.NoResultException;
 
 class JpaPOSRepository extends CafeteriaJpaRepositoryBase<POS, Long> implements POSRepository {
@@ -26,9 +30,13 @@ class JpaPOSRepository extends CafeteriaJpaRepositoryBase<POS, Long> implements 
         POS pos = findPOSByID(id).get();
 
         if (!pos.isOpen()) {
-
             //abre POS
             if (pos.open()) {
+                try {
+                    this.save(pos);
+                } catch (DataConcurrencyException | DataIntegrityViolationException ex) {
+                    Logger.getLogger(JpaPOSRepository.class.getName()).log(Level.SEVERE, null, ex);
+                }
                 return true;
             }
         }
