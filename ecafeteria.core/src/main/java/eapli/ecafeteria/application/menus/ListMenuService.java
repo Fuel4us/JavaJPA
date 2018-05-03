603e89dd545b0e33479abbbf8117e91b7d0322e0
Joao Reis (1160600) <1160600@isep.ipp.pt>
2018-05-03 17:57:57 +0100

    mealsByPeriod changed from listMenuService to ListmealService

--- ecafeteria.core/src/main/java/eapli/ecafeteria/application/menus/ListMenuService.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/application/menus/ListMenuService.java
@@ -7,17 +7,10 @@ package eapli.ecafeteria.application.menus;
 
 import eapli.ecafeteria.application.authz.AuthorizationService;
 import eapli.ecafeteria.domain.authz.ActionRight;
-import eapli.ecafeteria.domain.meals.Meal;
 import eapli.ecafeteria.domain.menus.Menu;
 import eapli.ecafeteria.persistence.MealRepository;
 import eapli.ecafeteria.persistence.MenuRepository;
 import eapli.ecafeteria.persistence.PersistenceContext;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Collections;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.List;
 
 /**
  *
@@ -32,67 +25,5 @@ public class ListMenuService {
         AuthorizationService.ensurePermissionOfLoggedInUser(ActionRight.MANAGE_MENUS);
 
         return this.menuRepository.findByState(false);
-    }
-
-    /**
-     * Joao Reis - 1160600
-     *
-     * @param beginning beginning of the time period
-     * @param end end of the time period
-     * @return a menu that either starts or ends on the specified time period
-     */
-    public static List<Meal> menuByPeriod(Date beginning, Date end) {
-        List<Meal> mealList = new ArrayList<>();
-        Iterable<Meal> mealsInMenu;
-        mealsInMenu = mealRepository.findByDatePeriod(beginning, end);
-        for (Meal meal : mealsInMenu) {
-            mealList.add(meal);
-        }
-
-        Collections.sort(mealList, Meal.compareDates());
-        return mealList;
-    }
-
-    /**
-     * Joao Reis - 1160600
-     *
-     * @return
-     */
-    public static List<Meal> menuForCurrentWeek() {
-        Date beginning, end, aux;
-        aux = new Date();
-
-        Calendar cal = new GregorianCalendar();
-        cal.setTime(aux);
-        while (cal.get(Calendar.DAY_OF_WEEK) > cal.getFirstDayOfWeek()) {
-            cal.add(Calendar.DATE, -1);
-        }
-        beginning = cal.getTime();
-        cal.add(Calendar.DATE, 7);
-        end = cal.getTime();
-
-        return menuByPeriod(beginning, end);
-    }
-
-    /**
-     * Joao Reis - 1160600
-     *
-     * @return
-     */
-    public static List<Meal> menuForNextWeek() {
-        Date beginning, end, aux;
-        aux = new Date();
-
-        Calendar cal = new GregorianCalendar();
-        cal.setTime(aux);
-        while (cal.get(Calendar.DAY_OF_WEEK) > cal.getFirstDayOfWeek()) {
-            cal.add(Calendar.DATE, -1);
-        }
-        cal.add(Calendar.DATE, 8);
-        beginning = cal.getTime();
-        cal.add(Calendar.DATE, 7);
-        end = cal.getTime();
-
-        return menuByPeriod(beginning, end);
     }
 }
