841b16939dc9afd0312876132d0b0eff3e838e5f
Tiago João Santos Rios <1161292@isep.ipp.pt>
2018-05-03 17:54:26 +0100

    Adição do método last() ao repositório de Heuristica

--- ecafeteria.persistence.impl/src/main/java/eapli/ecafeteria/persistence/jpa/JpaHeuristicRepository.java
+++ ecafeteria.persistence.impl/src/main/java/eapli/ecafeteria/persistence/jpa/JpaHeuristicRepository.java
@@ -3,6 +3,7 @@ package eapli.ecafeteria.persistence.jpa;
 import eapli.ecafeteria.domain.kitchen.HeuristicConfiguration;
 import eapli.ecafeteria.persistence.HeuristicRepository;
 import eapli.framework.persistence.repositories.impl.jpa.JpaAutoTxRepository;
+import java.util.Iterator;
 
 /**
  *
@@ -14,4 +15,14 @@ public class JpaHeuristicRepository extends JpaAutoTxRepository<HeuristicConfigu
         super(persistenceUnitName);
     }
     
+    public HeuristicConfiguration last(){
+        final Iterator<HeuristicConfiguration> it = findAll().iterator();
+        HeuristicConfiguration value = null;
+        
+        while(it.hasNext()){
+            value = it.next();
+        }
+        
+        return value;
+    }
 }
