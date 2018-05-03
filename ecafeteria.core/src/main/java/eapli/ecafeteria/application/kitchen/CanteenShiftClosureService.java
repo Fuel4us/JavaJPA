5ea54d67516cf819c804be61e12d0611c2662c59
Diogo Monteiro (1140302) <1140302@isep.ipp.pt>
2018-05-03 17:14:08 +0100

    [Implementation] Added attribute from class BookingRepository to CanteenShiftClosureService to have access to the Booking's present in the database

--- ecafeteria.core/src/main/java/eapli/ecafeteria/application/kitchen/CanteenShiftClosureService.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/application/kitchen/CanteenShiftClosureService.java
@@ -1,11 +1,13 @@
 package eapli.ecafeteria.application.kitchen;
 
+import eapli.ecafeteria.persistence.BookingRepository;
 import eapli.ecafeteria.persistence.ExecutionRepository;
 import eapli.ecafeteria.persistence.PersistenceContext;
 
 public class CanteenShiftClosureService {
 
-    final ExecutionRepository repository= PersistenceContext.repositories().execution();
+    final ExecutionRepository executionRepository= PersistenceContext.repositories().execution();
+    final BookingRepository bookingRepository = PersistenceContext.repositories().booking();
 
     public CanteenShiftClosureService(){
 
