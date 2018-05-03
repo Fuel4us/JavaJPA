048ef3b925d2f2acd98d4c1ff660727fe125766d
Diogo Monteiro (1140302) <1140302@isep.ipp.pt>
2018-05-03 17:47:48 +0100

    [Implementation] Added methods needed to complete Use Case 4.2.4.2 Functionality to save the information asked by this use case still not implemented.

--- ecafeteria.core/src/main/java/eapli/ecafeteria/application/kitchen/CanteenShiftClosureService.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/application/kitchen/CanteenShiftClosureService.java
@@ -1,15 +1,19 @@
 package eapli.ecafeteria.application.kitchen;
 
+import eapli.ecafeteria.domain.booking.Booking;
 import eapli.ecafeteria.domain.kitchen.Execution;
 import eapli.ecafeteria.domain.meals.Meal;
 import eapli.ecafeteria.persistence.BookingRepository;
 import eapli.ecafeteria.persistence.ExecutionRepository;
 import eapli.ecafeteria.persistence.PersistenceContext;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class CanteenShiftClosureService {
 
+    private CheckBookingsService service = new CheckBookingsService();
     final ExecutionRepository executionRepository= PersistenceContext.repositories().execution();
     final BookingRepository bookingRepository = PersistenceContext.repositories().booking();
 
@@ -32,5 +36,24 @@ public class CanteenShiftClosureService {
         return null;
     }
 
+    public List<Booking> getBookingListByMeal(Meal meal){
+        List<Booking> bookingList = new ArrayList<>();
+
+        for (Booking booking:
+             service.getAllReservations(2)) {
+            if(booking.getMeal().equals(meal)){
+                bookingList.add(booking);
+            }
+        }
+
+        return bookingList;
+    }
+
+    public void registerMealsDoneButNotSold(Meal meal){
+        Execution execution = getExecutionByMeal(meal);
+        List<Booking> bookingList = getBookingListByMeal(meal);
+        int mealDoneButNotSold = execution.getCookedMeals()-bookingList.size();
+        //register in database
+    }
 
 }
