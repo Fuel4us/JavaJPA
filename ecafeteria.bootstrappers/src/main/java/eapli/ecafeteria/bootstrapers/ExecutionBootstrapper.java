66a2cc7f306fbf7f56c017ee6adeb31ec2d7d6ba
Diogo Monteiro (1140302) <1140302@isep.ipp.pt>
2018-05-03 17:07:34 +0100

    [Implementation] Added Execution objects to the database bootstrap using the class ExecutionBootstrapper

--- ecafeteria.bootstrappers/src/main/java/eapli/ecafeteria/bootstrapers/ExecutionBootstrapper.java
+++ ecafeteria.bootstrappers/src/main/java/eapli/ecafeteria/bootstrapers/ExecutionBootstrapper.java
@@ -5,12 +5,16 @@
  */
 package eapli.ecafeteria.bootstrapers;
 
+import eapli.ecafeteria.domain.kitchen.Execution;
+import eapli.ecafeteria.domain.meals.Meal;
 import eapli.ecafeteria.persistence.ExecutionRepository;
 import eapli.ecafeteria.persistence.MealRepository;
 import eapli.ecafeteria.persistence.PersistenceContext;
 import eapli.framework.actions.Action;
 import eapli.framework.persistence.DataConcurrencyException;
 import eapli.framework.persistence.DataIntegrityViolationException;
+
+import java.util.Iterator;
 
 /**
  *
@@ -21,7 +25,19 @@ public class ExecutionBootstrapper implements Action {
     @Override
     public boolean execute() {
         try {
-            register();
+            final MealRepository mealRepository = PersistenceContext.repositories().meals();
+
+
+            Iterator<Meal> it = mealRepository.findAll().iterator();
+            final Meal meal1 = it.next();
+            final Meal meal2 = it.next();
+            final Meal meal3 = it.next();
+            Execution execution1= new Execution(meal1,40);
+            Execution execution2 = new Execution(meal2,60);
+            Execution execution3 = new Execution(meal3,100);
+            register(execution1);
+            register(execution2);
+            register(execution3);
         } catch (DataIntegrityViolationException | DataConcurrencyException ex) {
             return false;
         }
@@ -29,10 +45,8 @@ public class ExecutionBootstrapper implements Action {
         return true;
     }
     
-    public void register() throws DataIntegrityViolationException, DataConcurrencyException {
-        final MealRepository mealRepository = PersistenceContext.repositories().meals();
+    public void register(Execution execution) throws DataIntegrityViolationException, DataConcurrencyException {
         final ExecutionRepository executionRepository = PersistenceContext.repositories().execution();
-
-        
+        executionRepository.save(execution);
     }
 }
