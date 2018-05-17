c71849cf05c7dc6ff41157385b12f652857be280
Tiago Babo (1160760) <1160760@isep.ipp.pt>
2018-05-17 17:45:55 +0100

    Started working on use case

--- ecafeteria.core/src/main/java/eapli/ecafeteria/domain/cafeteriauser/NutritionalProfile.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/domain/cafeteriauser/NutritionalProfile.java
@@ -5,27 +5,35 @@
  */
 package eapli.ecafeteria.domain.cafeteriauser;
 
+import com.sun.org.apache.xml.internal.security.encryption.AgreementMethod;
 import eapli.ecafeteria.domain.dishes.Allergen;
+import eapli.framework.domain.ddd.AggregateRoot;
 import eapli.framework.domain.ddd.ValueObject;
 import java.io.Serializable;
 import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
-import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 /**
  *
  * @author pedromonteiro
  */
-@Embeddable
-public class NutritionalProfile implements ValueObject, Serializable{
+@Entity
+public class NutritionalProfile implements  Serializable, AggregateRoot<Long>{
+
+    @Id
+    private Long id;
     
     private static final long serialVersionUID = 1L;
     
     @Column(name = "allergen_list")
     @OneToMany(cascade = CascadeType.ALL)
     private Set<Allergen> allergenList;
+    
+    protected NutritionalProfile(){};
     
     public boolean addAllergen(Allergen allergen){
         return allergenList.add(allergen);
@@ -37,6 +45,29 @@ public class NutritionalProfile implements ValueObject, Serializable{
     
     public Iterable<Allergen> userAllergens(){
         return allergenList;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean sameAs(Object other) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean is(Long otherId) {
+        return AggregateRoot.super.is(otherId);
+    }
+
+    @Override
+    public Long id() {
+        return this.id;
     }
     
 }
