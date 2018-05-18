6e7cd9a9d4dd9fd42908c43ef3a3124d17fcb18c
Leandro Pinto (1160815) <1160815@isep.ipp.pt>
2018-05-18 14:25:46 +0100

    Creation of the Profile Entity, added profile to the cafeteria user

---
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/domain/cafeteriauser/Profile.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eapli.ecafeteria.domain.cafeteriauser;
+
+import java.io.Serializable;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author Leandro
+ */
+@Entity
+public class Profile implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    private final int DEFAULT_BALANCE_LIMIT = 0;
+    
+    
+    @Id
+    @GeneratedValue
+    private Long id;
+    
+    @Column(name = "balance_limit")
+    @OneToOne(cascade = CascadeType.ALL)
+    private int balanceLimit;
+    
+    protected Profile(){
+        this.balanceLimit = DEFAULT_BALANCE_LIMIT;
+    }
+    
+    /**
+     * Adds a new user Balance limit
+     * @param balanceLimit new balance limit
+     * @return True if a new balance is added, false if not
+     */
+    public boolean addBalanceLimit(int balanceLimit){
+        if(balanceLimit > 0){
+            this.balanceLimit = balanceLimit;
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    /**
+     * Return the current user balance limit
+     * @return Value of the current balance limit
+     */
+    public int getBalanceLimit(){
+        return balanceLimit;
+    }
+    
+}
