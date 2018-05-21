151242b9ce28a4518454a95fda42700c22b70570
Gonçalo Silva (1161140) <1161140@isep.ipp.pt>
2018-05-22 00:02:24 +0100

    Started heuristics implementation

--- ecafeteria.core/src/main/java/eapli/ecafeteria/domain/kitchen/HeuristicA.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/domain/kitchen/HeuristicA.java
@@ -1,28 +1,44 @@
 package eapli.ecafeteria.domain.kitchen;
 
-import eapli.framework.presentation.console.ShowMessageAction;
+import eapli.ecafeteria.application.kitchen.GenerateMealPlanController;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Embeddable;
 
 /**
+ * LastAvailableWeek - Copies the number of meals from last week
  *
- * @author joaotiagow
+ * @author Tiago João Santos Rios, 1161292@isep.ipp.pt
+ * @author Gonçalo Silva (1161140)
  */
 @Embeddable
 public class HeuristicA implements Heuristic {
-    
+
     private String name;
-    
-    public HeuristicA(String name){
+    private GenerateMealPlanController controller;
+
+    public HeuristicA(String name) {
         this.name = name;
     }
 
     @Override
     public void doHeuristicLogic() {
-        new ShowMessageAction("Under Development").execute();
+        List<MealPlanItemQuantity> mealPlanItemQuantityList = new ArrayList<>();
+
+        //Criar método para ir buscar apenas a última mealPlan / última semana
+        for (MealPlan mealPlan : controller.getMealPlanHistory()) {
+            mealPlanItemQuantityList = mealPlan.getItemQuantityList();
+        }
+
+        returnToController(mealPlanItemQuantityList);
     }
-    
+
+    public List<MealPlanItemQuantity> returnToController(List<MealPlanItemQuantity> mealPlanItemQuantityList) {
+        return mealPlanItemQuantityList;
+    }
+
     @Override
-    public String toString(){
+    public String toString() {
         return this.name;
     }
 }
--- ecafeteria.core/src/main/java/eapli/ecafeteria/domain/kitchen/HeuristicB.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/domain/kitchen/HeuristicB.java
@@ -1,28 +1,56 @@
 package eapli.ecafeteria.domain.kitchen;
 
-import eapli.framework.presentation.console.ShowMessageAction;
+import eapli.ecafeteria.application.kitchen.GenerateMealPlanController;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Embeddable;
 
 /**
+ * Average - Averages the number of meals to cook
  *
- * @author joaotiagow
+ * @author Tiago João Santos Rios, 1161292@isep.ipp.pt
+ * @author Gonçalo Silva (1161140)
  */
 @Embeddable
 public class HeuristicB implements Heuristic {
-    
+
     private String name;
-    
-    public HeuristicB(String name){
+    private GenerateMealPlanController controller;
+
+    public HeuristicB(String name) {
         this.name = name;
     }
 
     @Override
     public void doHeuristicLogic() {
-        new ShowMessageAction("Under Development").execute();
+        List<MealPlanItemQuantity> mealPlanItemQuantityList = new ArrayList<>();
+        List<Integer> averageMealsList = new ArrayList<>();
+        int totalMeals = 0, averageMeals = 0;
+
+        for (MealPlan mealPlan : controller.getMealPlanHistory()) {
+            mealPlanItemQuantityList = mealPlan.getItemQuantityList();
+
+            for (MealPlanItemQuantity mealPlanItemQuantity : mealPlanItemQuantityList) {
+                int itemQuantity = mealPlanItemQuantity.getItemQuantity();
+
+                totalMeals += itemQuantity;
+            }
+        }
+
+        averageMeals = totalMeals / mealPlanItemQuantityList.size();
+
+        //Passar isto corretamente para um array de int
+        averageMealsList.add(averageMeals);
+
+        returnToController(averageMealsList);
     }
-    
+
+    public List<Integer> returnToController(List<Integer> averageMealsList) {
+        return averageMealsList;
+    }
+
     @Override
-    public String toString(){
+    public String toString() {
         return this.name;
     }
 }
