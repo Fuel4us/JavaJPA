0608762aa45766dd7cc3e4919c7d238b739ba8e6
Gonçalo Silva (1161140) <1161140@isep.ipp.pt>
2018-05-21 12:05:55 +0100

    Updated MealPlanBootstrapper to add MealPlanItemQuantity

--- ecafeteria.bootstrappers/src/main/java/eapli/ecafeteria/bootstrapers/MealPlanBootstrapper.java
+++ ecafeteria.bootstrappers/src/main/java/eapli/ecafeteria/bootstrapers/MealPlanBootstrapper.java
@@ -1,13 +1,18 @@
 package eapli.ecafeteria.bootstrapers;
 
+import eapli.ecafeteria.application.kitchen.CreateMealPlanController;
 import eapli.ecafeteria.domain.kitchen.MealPlan;
+import eapli.ecafeteria.domain.kitchen.MealPlanItemQuantity;
+import eapli.ecafeteria.domain.meals.Meal;
 import eapli.ecafeteria.domain.menus.Menu;
-import eapli.ecafeteria.persistence.MealPlanRepository;
 import eapli.ecafeteria.persistence.MenuRepository;
 import eapli.ecafeteria.persistence.PersistenceContext;
 import eapli.framework.actions.Action;
 import eapli.framework.persistence.DataConcurrencyException;
 import eapli.framework.persistence.DataIntegrityViolationException;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
 /**
  * @author Tiago Babo 1160760
@@ -28,12 +33,19 @@ public class MealPlanBootstrapper implements Action {
 
     public void register() throws DataIntegrityViolationException, DataConcurrencyException {
         final MenuRepository menuRepository = PersistenceContext.repositories().menus();
-        final MealPlanRepository mealLotRepository = PersistenceContext.repositories().mealplans();
+        final CreateMealPlanController controller = new CreateMealPlanController();
 
         for (Menu menu : menuRepository.findAll()) {
-            MealPlan mealPlan = new MealPlan(menu);
+            MealPlan mealPlan = controller.createMealPlan(menu);
 
-            mealLotRepository.save(mealPlan);
+            for (Meal meal : controller.getMealList(mealPlan)) {
+                Random rand = new Random();
+
+                MealPlanItemQuantity mpiq = controller.createMealPlanItemQuantity(rand.nextInt(10) + 1, meal);
+                controller.getItemQuantityList(mealPlan).add(mpiq);
+            }
+
+            controller.saveMealPlan(mealPlan);
         }
     }
 }
