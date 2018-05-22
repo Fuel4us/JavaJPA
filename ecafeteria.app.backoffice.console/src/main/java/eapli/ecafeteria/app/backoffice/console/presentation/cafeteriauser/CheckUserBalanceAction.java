8fb049138e4b4aa4648f03cde0b78d816f6d7193
Gonçalo Fonseca (1150503) <1150503@isep.ipp.pt>
2018-05-23 00:06:38 +0100

    [Implementation] Action implemented

---
+++ ecafeteria.app.backoffice.console/src/main/java/eapli/ecafeteria/app/backoffice/console/presentation/cafeteriauser/CheckUserBalanceAction.java
@@ -0,0 +1,13 @@
+package eapli.ecafeteria.app.backoffice.console.presentation.cafeteriauser;
+
+import eapli.framework.actions.Action;
+
+/**
+ * @author Gonçalo Fonseca <1150503@isep.ipp.pt>
+ */
+public class CheckUserBalanceAction implements Action {
+    @Override
+    public boolean execute() {
+        return new CheckUserBalanceUI().show();
+    }
+}
