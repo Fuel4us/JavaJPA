5390464b3b9c0c9341c0e677c79f2aab36d5b236
João Pires (1150455) <1150455@isep.ipp.pt>
2018-05-23 17:30:22 +0100

    CheckDishRatingController method listDishes

--- ecafeteria.core/src/main/java/eapli/ecafeteria/application/dishes/CheckDishRatingController.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/application/dishes/CheckDishRatingController.java
@@ -5,7 +5,14 @@
  */
 package eapli.ecafeteria.application.dishes;
 
+import eapli.ecafeteria.domain.dishes.Dish;
+import eapli.ecafeteria.persistence.DishRepository;
+import eapli.ecafeteria.persistence.PersistenceContext;
+import eapli.ecafeteria.persistence.RatingRepository;
 import eapli.framework.application.Controller;
+import java.util.HashMap;
+import java.util.Map;
+import javax.print.attribute.HashAttributeSet;
 
 /**
  *
@@ -13,17 +20,21 @@ import eapli.framework.application.Controller;
  */
 public class CheckDishRatingController implements Controller {
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    private final DishRepository dishRepo = PersistenceContext.repositories().dishes();
+
+    private final RatingRepository ratingRepo = PersistenceContext.repositories().rating();
+
+    public Iterable<Dish> listDishes() {
+        return dishRepo.findAll();
+    }
+
+    public Map<Dish, Integer> getRatingPerDish(Dish dish) {
+
+        Map<Dish, Integer> map = new HashMap<>();
+
+        int avg;
+
+        return map;
+    }
+
 }
