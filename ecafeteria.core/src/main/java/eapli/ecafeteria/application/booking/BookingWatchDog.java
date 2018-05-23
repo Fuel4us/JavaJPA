e0a84ecaee8f87b940a10c57d716be26c0dd2bfc
Pedro Rodrigues (1140572) <1140572@isep.ipp.pt>
2018-05-23 20:34:22 +0100

    BookingWatchDog updated. MealPlanServices implemented.

--- ecafeteria.core/src/main/java/eapli/ecafeteria/application/booking/BookingWatchDog.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/application/booking/BookingWatchDog.java
@@ -6,7 +6,7 @@
 package eapli.ecafeteria.application.booking;
 
 import eapli.ecafeteria.application.administration.KitchenLimitsServices;
-import eapli.ecafeteria.application.meals.MealServices;
+import eapli.ecafeteria.application.meals.MealPlanServices;
 import eapli.ecafeteria.domain.booking.Booking;
 import eapli.ecafeteria.domain.meals.Meal;
 import java.util.Observable;
@@ -20,12 +20,22 @@ public class BookingWatchDog extends Observable implements Observer {
 
     private BookingServices bookingServices = new BookingServices();
     private KitchenLimitsServices kitchenLimitsServices = new KitchenLimitsServices();
-    private MealServices mealServices = new MealServices();
-    
+    private MealPlanServices mealPlanServices = new MealPlanServices();
+
     private int numberOfMeal = 0;
     private int numberOfBookingByMeal = 0;
-    private double yellowLimit;
-    private double redLimit;
+    private double yellowLimit = 0;
+    private double redLimit = 0;
+
+    public void getNumberOfBookingByMeal(Meal meal) {
+        for (Booking booking : bookingServices.findByMeal(meal)) {
+            numberOfBookingByMeal++;
+        }
+    }
+
+    public void getNumberOfMeal(Meal meal) {
+        numberOfMeal = mealPlanServices.findQuantityByMeal(meal);
+    }
 
     public void getYellowLimit() {
         yellowLimit = kitchenLimitsServices.findYellowLimit();
@@ -35,17 +45,14 @@ public class BookingWatchDog extends Observable implements Observer {
         redLimit = kitchenLimitsServices.findRedLimit();
     }
 
-    public void getNumberOfMeal(Meal meal) {
-        numberOfMeal = 0;
-    }
-
-    public void getNumberOfBookingByMeal(Meal meal) {
-        for (Booking booking : bookingServices.findByMeal(meal)) {
-            numberOfBookingByMeal++;
-        }
-    }
-
     @Override
     public void update(Observable o, Object o1) {
+        if ((numberOfBookingByMeal / numberOfMeal) * 100 > yellowLimit && (numberOfBookingByMeal / numberOfMeal) * 100 < redLimit) {
+            setChanged();
+            notifyObservers("As reservas ultrapassaram o limite amarelo de " + yellowLimit + "%.");
+        } else if ((numberOfBookingByMeal / numberOfMeal) * 100 > redLimit) {
+            setChanged();
+            notifyObservers("As reservas ultrapassaram o limite vermelho de " + redLimit + "%.");
+        }
     }
 }
