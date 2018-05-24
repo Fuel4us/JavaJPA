9810eef76fb2931bb5c21a5e720c35458d1b2810
Diogo Monteiro (1140302) <1140302@isep.ipp.pt>
2018-05-24 02:43:16 +0100

    [Implementation] Created Class Picked as a value object to be used in Execution

---
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/domain/kitchen/Picked.java
@@ -0,0 +1,42 @@
+package eapli.ecafeteria.domain.kitchen;
+
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+public class Picked {
+    private int pickedQuantity;
+
+    public Picked(int pickedQuantity) {
+        this.pickedQuantity = pickedQuantity;
+    }
+
+    public int getPickedQuantity() {
+        return pickedQuantity;
+    }
+
+    public void setPickedQuantity(int pickedQuantity) {
+        this.pickedQuantity = pickedQuantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Picked picked = (Picked) o;
+        return pickedQuantity == picked.pickedQuantity;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(pickedQuantity);
+    }
+
+    @Override
+    public String toString() {
+        return "Picked{" +
+                "pickedQuantity=" + pickedQuantity +
+                '}';
+    }
+}
