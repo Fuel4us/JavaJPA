a36c2d10e2b195f6e4e851a4641473924455ced2
PedroVieira1160634 <1160634@isep.ipp.pt>
2018-05-24 01:04:33 +0100

    Comment and Rate - Tests

--- ecafeteria.core/src/main/java/eapli/ecafeteria/domain/booking/Comment.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/domain/booking/Comment.java
@@ -14,6 +14,9 @@ import javax.persistence.Embeddable;
 @Embeddable
 public class Comment implements Serializable, ValueObject {
 
+    /*
+    * The default answer, before the menu manager reply
+    */
     private static final String INITIAL_ANSWER = "there is no answer yet!";
     private static final long serialVersionUID = 1L;
     
