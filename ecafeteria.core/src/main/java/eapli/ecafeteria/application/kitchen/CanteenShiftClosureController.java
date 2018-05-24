57859c6419f325d14f93f1fc2e779ffc8d53b741
Diogo Monteiro (1140302) <1140302@isep.ipp.pt>
2018-05-24 03:40:45 +0100

    [Implementation] Added remaining code for Use Case Completion Use Case done successfully. Might be changed in the future for better organization

--- ecafeteria.core/src/main/java/eapli/ecafeteria/application/kitchen/CanteenShiftClosureController.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/application/kitchen/CanteenShiftClosureController.java
@@ -4,13 +4,16 @@ import eapli.ecafeteria.application.authz.AuthorizationService;
 import eapli.ecafeteria.domain.authz.ActionRight;
 import eapli.ecafeteria.domain.finance.POS;
 import eapli.ecafeteria.domain.kitchen.CanteenShift;
+import eapli.ecafeteria.domain.kitchen.Execution;
 import eapli.ecafeteria.domain.meals.Meal;
-import eapli.ecafeteria.persistence.CanteenShiftRepository;
-import eapli.ecafeteria.persistence.POSRepository;
-import eapli.ecafeteria.persistence.PersistenceContext;
+import eapli.ecafeteria.persistence.*;
 import eapli.framework.application.Controller;
 import eapli.framework.persistence.DataConcurrencyException;
 import eapli.framework.persistence.DataIntegrityViolationException;
+import eapli.framework.util.DateTime;
+
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -20,6 +23,9 @@ public class CanteenShiftClosureController implements Controller {
     private Meal meal;
     private final POSRepository posRepository = PersistenceContext.repositories().POS();
     private final CanteenShiftRepository csRepository = PersistenceContext.repositories().canteenShift();
+    private final ExecutionRepository executionRepository = PersistenceContext.repositories().execution();
+    private final MealRepository mealRepository = PersistenceContext.repositories().meals();
+    private final MealPlanItemQuantityRepository mealPlanItemQuantityRepository = PersistenceContext.repositories().mealplanitemquantities();
 
     /**
      * It does not return false if there are no open POS 
@@ -48,6 +54,26 @@ public class CanteenShiftClosureController implements Controller {
             return false;
         
         try {
+
+            Date date = new java.sql.Date(Calendar.getInstance().getTimeInMillis());
+
+           
+            for (Meal meal:
+                 mealRepository.findMealByDate(date)) {
+                Execution execution;
+                Iterable<Execution>it = executionRepository.getExecutionByMeal(meal);
+                if(!it.iterator().hasNext()){
+                    System.out.println("Execution of the meal not found");
+                }else {
+                    int mealsPicked = executionRepository.getPickedMeals(meal);
+                    int mealsActuallyDone = executionRepository.getMealActuallyServed(meal);
+
+                    execution = it.iterator().next();
+                    execution.getLeftover().setQuantity(mealsActuallyDone - mealsPicked);
+                    executionRepository.save(execution);
+                }
+            }
+
             csRepository.save(currentCanteenShift);
         } catch (DataConcurrencyException | DataIntegrityViolationException ex) {
             Logger.getLogger(CanteenShiftClosureController.class.getName()).log(Level.SEVERE, null, ex);
